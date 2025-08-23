f2f6630063ea463bae7d67ca49e1c4b58f8e66b1
edgar hugo <022200901g@uandina.edu.pe>
2025-08-23 10:06:16 -0500

    avance2

--- lib/screeens/tabs_screen.dart
+++ lib/screeens/tabs_screen.dart
@@ -5,19 +5,18 @@ class TabsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return DefaultTabController(
-      length: 4, 
+      length: 10, 
       child: Scaffold(
         body: SafeArea(
           child: Column(
             children: const [
               TabBar(
+                isScrollable: true,
                 labelColor: Colors.black,
                 unselectedLabelColor: Colors.grey,
                 tabs: [
-                  Tab(text: 'Tab 1'),
-                  Tab(text: 'Tab 2'),
-                  Tab(text: 'Tab 3'),
-                  Tab(text: 'Tab 4'),
+                  Tab(text: 'Tab 1'),Tab(text: 'Tab 2'),Tab(text: 'Tab 3'),Tab(text: 'Tab 4'),Tab(text: 'Tab 5'),
+                  Tab(text: 'Tab 6'),Tab(text: 'Tab 7'),Tab(text: 'Tab 8'),Tab(text: 'Tab 9'),Tab(text: 'Tab 10'),
                 ]),
               Expanded(
                 child: TabBarView(children: [
@@ -25,6 +24,13 @@ class TabsScreen extends StatelessWidget{
                   Icon(Icons.directions_transit_filled_outlined,size: 100,),
                   Icon(Icons.directions_bike_sharp,size: 100,),
                   Icon(Icons.directions_walk_sharp,size: 100,),
+                  Icon(Icons.directions_railway_filled,size: 100,),
+                  Icon(Icons.directions_off_outlined,size: 100,),
+                  Icon(Icons.directions_walk_rounded,size: 100,),
+                  Icon(Icons.directions_railway,size: 100,),
+                  Icon(Icons.directions_subway_filled_rounded,size: 100,),
+                  Icon(Icons.directions_bus_rounded,size: 100,),
+
                 ]),
                 )  
             ],
